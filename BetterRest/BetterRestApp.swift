2aa1dada06e886498912178cbad0bf3f6c5d9bd0
Connor Ashton <cjconnorashton@gmail.com>
2023-10-09 13:27:29 -0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Connor Ashton on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
